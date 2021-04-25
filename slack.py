0ce12e1ae66f2d45efc54088b3f55d08b2cec2e6
GwiHwan-Go <83144588+GwiHwan-Go@users.noreply.github.com>
2021-04-25 09:55:45 +0900

    Create slack.py

---
+++ slack.py
@@ -0,0 +1,15 @@
+#send slack
+#!pip install requests
+import requests
+ 
+def post_message(token, channel, text):
+    response = requests.post("https://slack.com/api/chat.postMessage",
+        headers={"Authorization": "Bearer "+token},
+        data={"channel": channel,"text": text}
+    )
+    print(response)
+ 
+myToken = "bot user oauth token"
+message = "send what you want"
+
+post_message(myToken,"#crypto-auto-trading",message)
